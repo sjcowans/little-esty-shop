403dca448f07491863001789ba47605cb73c61e4
Sean Cowans <zedesia@yahoo.com>
2023-04-19 16:17:44 -0600

    fix: Hopefully last fix for Heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-if ENV["RAILS_LOG_TO_STDOUT"].present?
-  logger           = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
-end
