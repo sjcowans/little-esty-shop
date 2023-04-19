dff2610ae023fd060f3a83ebdb44843484291be2
Sean Cowans <zedesia@yahoo.com>
2023-04-19 17:00:13 -0600

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/merchants/:id/dashboard', to: 'merchants#show'
-  
+  get '/', to: 'welcome#index'
   get '/admin', to: 'admin#index' 
 
   resources :merchants do
