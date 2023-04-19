cb40af941c6c9915ee048da51d27a302680cef83
Sean Cowans <zedesia@yahoo.com>
2023-04-19 17:43:00 -0600

    Render please....

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+InvoiceItem.destroy_all
 Transaction.destroy_all
 Invoice.destroy_all
 Customer.destroy_all
