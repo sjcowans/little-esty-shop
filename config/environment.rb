ac8dd78b38e775718bfc9ca545b94c1982630c39
Sean Cowans <zedesia@yahoo.com>
2023-04-19 16:13:39 -0600

    fix: Fix config for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,6 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
 if ENV["RAILS_LOG_TO_STDOUT"].present?
   logger           = ActiveSupport::Logger.new(STDOUT)
   logger.formatter = config.log_formatter
